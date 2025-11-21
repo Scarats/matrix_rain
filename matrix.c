0062ae1f796b4f899ab5deafe3443ce8c8ce7142
Scarats.42 <0xscarats@gmail.com>
2025-11-21 15:14:05 +0100

    size limit to avoid int oversize and allocation segfault

--- matrix.c
+++ matrix.c
@@ -12,14 +12,14 @@
 # define RESET "\x1B[0m"
 # define MATRIX 100
 
-volatile sig_atomic_t stop_flag;
+volatile sig_atomic_t g_stop_flag;
 
 // Read from random, else generate an int from a memory address.
-int gen_random(int size)
+int	gen_random(int size)
 {
-	int fd;
-	int seed;
-	char *addr;
+	int		fd;
+	int		seed;
+	char	*addr;
 
 	if (size == 0)
 		size = MATRIX;
@@ -43,11 +43,11 @@ int gen_random(int size)
 }
 
 // Apply random numbers within height and -height to an array of int.
-void gen_matrix(int *array, int width, int height)
+void	gen_matrix(int *array, int width, int height)
 {
-	int i;
-	int prev;
-	int range;
+	int	i;
+	int	prev;
+	int	range;
 
 	i = 1;
 	array[0] = gen_random(width);
@@ -65,15 +65,14 @@ void gen_matrix(int *array, int width, int height)
 }
 
 // Add random char if array[i] is positive, else add space.
-void fill_buff(int *array, char *buff, int width, int height)
+void	fill_buff(int *array, char *buff, int width, int height)
 {
-	int i;
-	int r;
-	int ch;
-	const int printable_count = 126 - 33 + 1;
+	int			i;
+	int			r;
+	const int	printable_count = 126 - 33 + 1;
 
-	i = 0;
-	while (i < width)
+	i = -1;
+	while (++i < width)
 	{
 		if (array[i] > 0)
 		{
@@ -82,8 +81,7 @@ void fill_buff(int *array, char *buff, int width, int height)
 				r = -r;
 			if (r == 0)
 				r = 1;
-			ch = 33 + (r % printable_count);
-			buff[i] = (char)ch;
+			buff[i] = (char)(33 + (r % printable_count));
 		}
 		else
 			buff[i] = ' ';
@@ -91,17 +89,16 @@ void fill_buff(int *array, char *buff, int width, int height)
 			array[i] = -height;
 		else
 			array[i] += 1;
-		i++;
 	}
 	buff[width] = '\0';
 }
 
-void print_matrix(int *array, char *buff, int width, int height)
+void	print_matrix(int *array, char *buff, int width, int height)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while (!stop_flag)
+	while (!g_stop_flag)
 	{
 		fill_buff(array, buff, width, height);
 		printf(GREEN "%s\n" RESET, buff);
@@ -116,30 +113,29 @@ void print_matrix(int *array, char *buff, int width, int height)
 	}
 }
 
-void handler(int sig)
+void	matrix_rest(int height, int width, int *array, char *buff)
 {
-	(void)sig;
-    stop_flag = 1;
+	if (height == 0)
+		height = MATRIX;
+	printf(GREEN "\n\nFollow the white rabbit.\n\n" RESET);
+	printf(GREEN "\n\nKnock, knock, Neo.\n\n" RESET);
+	gen_matrix(array, width, height);
+	print_matrix(array, buff, width, height);
+	printf(GREEN "\n\nWake Up, Neo...\n\n" RESET);
+	free(array);
+	free(buff);
 }
 
 // Usage:
 // matrix width height
-int main(int argc, char **argv)
+int	main(int ac, char **argv)
 {
-    struct sigaction sa;
-	int width;
-	int height;
-	int *array;
-	char *buff;
+	int		width;
+	int		height;
+	int		*array;
+	char	*buff;
 
-	(void)argc;
-
-    sa.sa_handler = handler;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-
-    sigaction(SIGINT, &sa, NULL);
-
+	(void)ac;
 	buff = NULL;
 	if (argv[1])
 		width = atoi(argv[1]);
@@ -150,27 +146,14 @@ int main(int argc, char **argv)
 		width %= MATRIX;
 		free(buff);
 	}
-	if (width < 1)
+	if (width < 1 || width > 10000)
 		width = MATRIX;
-
 	array = malloc(width * sizeof(*array));
 	buff = malloc(width + 1);
-
-	if (argv[2])
+	if (argv[2] && (atoi(argv[2]) > 0 && atoi(argv[2]) < 10000))
 		height = atoi(argv[2]);
 	else
 		height = MATRIX;
-	if (height < 1)
-		height = MATRIX;
-
-	printf(GREEN "\n\nFollow the white rabbit.\n\n" RESET);
-	printf(GREEN "\n\nKnock, knock, Neo.\n\n" RESET);
-	gen_matrix(array, width, height);
-	print_matrix(array, buff, width, height);
-
-	printf(GREEN "\n\nWake Up, Neo...\n\n" RESET);
-
-	free(array);
-	free(buff);
+	matrix_rest(height, width, array, buff);
 	return (0);
 }
